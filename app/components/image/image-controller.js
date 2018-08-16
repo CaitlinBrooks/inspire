035a8f71f132a9a2a5cdbfc7727e4168987082da
CaitlinCodes <caiit@me.com>
2018-08-16 16:40:22 -0600

    controllers and services

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -3,19 +3,18 @@ import ImageService from "./image-service.js"
 
 let imageService = new ImageService
 
-// function getImages(image) {
-//   console.log(image)
-//   let template = ''
-//   // for (let i = 0; i < images.length; i++) {
-//   //   let image = images[i];
-//     template += `
-//     <div class="image">
-//     <img src="${image.imgUrl}" alt="imageUrl">
-//     </div>
-//     `
-//   }
-//   //maybe write body instead of images to target?
-// }
+function drawImage(image) {
+  console.log(image)
+  // let template = ''
+  // // for (let i = 0; i < images.length; i++) {
+  // //   let image = images[i];
+  // template += `
+  //   <div class="image">
+  //   <img src="${image.imgUrl}" alt="imageUrl">
+  //   </div>
+  //   `
+}
+//maybe write body instead of images to target?
 
 // document.getElementById('body').innerHTML = template
 
--- app/components/image/image-service.js
+++ app/components/image/image-service.js
@@ -2,7 +2,6 @@ const url = '//bcw-getter.herokuapp.com/?url=';
 const url2 = 'http://www.splashbase.co/api/v1/images/random/'
 const apiUrl = url + encodeURIComponent(url2);
 
-
 // @ts-ignore
 const imgApi = axios.create({
 	baseURL: apiUrl,
@@ -10,8 +9,12 @@ const imgApi = axios.create({
 });
 
 export default class ImageService {
+	constructor() {
+
+	}
+
 	getImage(callWhenDone) {
-		imgApi.get('images') //ask about this
+		imgApi.get() //ask about this
 		// ^^^^^^^ How do you call this function?
 		console.log("Looking for a good pic")
 		imgApi().then(res => {
