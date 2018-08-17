7b7ef450a7abcf311c6f41fad05780cecb5b538b
CaitlinCodes <caiit@me.com>
2018-08-17 10:50:20 -0600

    quote draw on page

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -3,9 +3,11 @@ import QuoteService from "./quote-service.js";
 let quoteService = new QuoteService
 
 function drawQuote(quote) {
-	console.log(quote)
-	document.getElementById('').innerHTML = `url('${quote.quote}')`
-	document.getElementById('').innerHTML = `url('${quote.author}')`
+	// console.log(quote)
+	document.getElementById('quote').innerHTML = `
+	<p class="quote">${quote.quote}</p>
+	<p class="quote">${quote.author}</p>
+	`
 }
 
 export default class QuoteController {
@@ -14,8 +16,10 @@ export default class QuoteController {
 	}
 
 	getQuote() {
+		console.log('quotecontroller created')
 		quoteService.getQuote(function (quote) {
 			console.log('What is the quote', quote)
+			quoteService.getQuote(drawQuote)
 		})
 	}
 }
